e596d0ebe1f002014da48314f429db790da9fca1
Czarek <karasoi@poczta.onet.pl>
2016-03-30 15:10:04 +0200

    Add first_name and user_name to devise new user view and application controller

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "User sign in/ sign up" do
+  let!(:user) { FactoryGirl.build(:user) }
+
+  scenario "Successfully sign up" do
+    visit new_user_registration_path
+
+    fill_in "First name", with: user.first_name
+    fill_in "Last name", with: user.last_name
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password
+
+    click_on "Sign up"
+
+    expect(page).to have_content user.email
+    expect(page).to have_content user.first_name
+
+  end
+end
