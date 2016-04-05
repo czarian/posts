6c360cf6d4f2248b5d9057eccb8c7f180dcddab7
Czarek <karasoi@poczta.onet.pl>
2016-04-05 22:29:06 +0200

    set env region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,16 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET']
+      :aws_secret_access_key  => ENV['S3_SECRET'],
       :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET']
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
 end
+
