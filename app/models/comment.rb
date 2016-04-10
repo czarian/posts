c81c39bac61576d56d0c16b08a5e78a4802e11fc
Czarek <karasoi@poczta.onet.pl>
2016-04-10 22:43:04 +0200

    Post, comment user validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   validates :body, presence: true
+  validates :user, presence: true
+  validates :post, presence: true
 end
